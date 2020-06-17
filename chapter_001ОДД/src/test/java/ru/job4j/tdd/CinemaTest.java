782355aec661d50ab1b5291f8a610cbd8abb2255
pavelchupin9416 <pavelchupin94@yandex.ru>
2020-06-17 22:55:00 +0300

    2. Что такое TDD?

---
+++ chapter_001ОДД/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.tdd;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    /**
+     * Тестирование покупки билета на границе вводимых данных. В методе пытаюсь
+     * купить 0 ряд и место.
+     */
+    @Test
+    public void buyBorder() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 0, 0, date);
+        assertThat(ticket, is(null));
+    }
+
+
+    /**
+     * Тестирование с указанием прошедшей даты.
+     */
+   @Test
+    public void buyDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2005, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(null));
+    }
+
+    /**
+     * Тестирование на отсутствие одного или несколько параметров.
+     */
+    @Test
+    public void buyNull() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2005, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, null, date);
+        assertThat(ticket, is(null));
+    }
+
+}
