da1c2ea02b134f444f01694021d0f3f59ba11fb7
pavelchupin9416 <pavelchupin94@yandex.ru>
2022-06-21 16:39:11 +0300

    2. Статистика по коллекции. [#45889]

---
+++ chapter_002Collections. Pro/src/main/java/ru/job4j/statistics/Analize.java
@@ -0,0 +1,35 @@
+package ru.job4j.statistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *Class Analize статистика по коллекции.
+ *@author Chupin Pavel
+ *@since 21.06.2022
+ */
+
+public class Analize {
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added;
+        int changed = 0;
+        int deleted = 0;
+        Map <Integer,String> map = new HashMap<>();
+        for (User cur : current) {
+            map.put(cur.getId(), cur.getName());
+        }
+        Set<Integer> keys= map.keySet();
+        for (User pre : previous) {
+            if (keys.contains(pre.getId())) {
+                if(!pre.getName().equals(map.get(pre.getId()))){
+                    changed=changed+1;
+                }
+            } else {
+             deleted=deleted+1;
+            }
+        }
+        added = current.size() - (previous.size()-deleted);
+        return new Info(added,changed,deleted);
+    }
+}
