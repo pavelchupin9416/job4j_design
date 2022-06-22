7ffb381439d0aebd531a8c51c1c808dc25293203
pavelchupin9416 <pavelchupin94@yandex.ru>
2022-06-22 17:30:42 +0300

    0.1. FileOutputStream. [#252488]

---
+++ chapter_003Input_Output/src/main/java/outputStream/ResultFile.java
@@ -0,0 +1,25 @@
+package outputStream;
+
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
+public class ResultFile {
+    public static void main(String[] args) {
+
+        Integer[][] multipTabl = new Integer[10][10];
+        for (int cell = 0; cell < multipTabl.length; cell++){
+            for (int row = 0; row < multipTabl[cell].length; row++) {
+                multipTabl[cell][row] = (cell + 1) * (row + 1);
+            }
+        }
+
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (Integer[] number: multipTabl) {
+                out.write(Arrays.deepToString(number).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
