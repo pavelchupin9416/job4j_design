2e503371b39bb684839ccddf2b3dd10c2293587c
pavelchupin9416 <pavelchupin94@yandex.ru>
2022-06-23 14:16:21 +0300

    0.4. BufferedOutputStream [#252490]

--- chapter_003Input_Output/src/main/java/bufferedReader/LogFilter.java
+++ chapter_003Input_Output/src/main/java/bufferedReader/LogFilter.java
@@ -1,8 +1,6 @@
 package bufferedReader;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,10 +17,24 @@ public class LogFilter {
         return result;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for(String element : log) {
+                out.println(element);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
         log.forEach(System.out :: println);
-
+        save(log,"log404.txt");
     }
 }
