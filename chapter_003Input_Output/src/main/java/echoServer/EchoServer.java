6f2199eb8b98cafbb2424970c4fdde8887e167b4
pavel.chupin <pavelchupin94@yandex.ru>
2022-07-08 14:19:32 +0300

    Фикс 2. Что такое Socket? [#4850]

--- chapter_003Input_Output/src/main/java/echoServer/EchoServer.java
+++ chapter_003Input_Output/src/main/java/echoServer/EchoServer.java
@@ -15,7 +15,7 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if(str.equals("GET /?msg=Bye HTTP/1.1")){
+                        if("GET /?msg=Bye HTTP/1.1".equals(str)){
                             server.close();
                         }
                     }
