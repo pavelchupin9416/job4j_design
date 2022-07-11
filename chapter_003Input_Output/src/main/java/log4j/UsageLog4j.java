055681bd9c2518d51a4bd79feeed974a4fd58cb2
pavel.chupin <pavelchupin94@yandex.ru>
2022-07-11 15:09:44 +0300

    3. Slf4j - вывод переменных. [#268852]

--- chapter_003Input_Output/src/main/java/log4j/UsageLog4j.java
+++ chapter_003Input_Output/src/main/java/log4j/UsageLog4j.java
@@ -3,15 +3,28 @@ package log4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class UsageLog4j {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
-    public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+    public static void main(String[] args) throws ParseException {
+        String name = "Pavel Chupin";
+        char gender = 'М';
+        boolean human = true;
+        byte age = 27;
+        short fingers = 20;
+        int foot_size=45;
+        long teeth = 28;
+        double weight =  98.1;
+        float growth = 183.5F;
+        Date birthDate = new SimpleDateFormat( "dd.MM.yyyy" ).parse( "16.11.1994" );
+
+        LOG.debug("User info name : {}, gender : {}, human : {}, age : {}, " +
+                "fingers : {}, foot_size : {}, teeth : {}, weight : {}, growth : {}, birthDate : {}",
+                name,gender, human, age, fingers, foot_size, teeth, weight, growth, birthDate);
     }
 }
