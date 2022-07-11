f7837359c9931d4454f70015e1417ed041708792
pavel.chupin <pavelchupin94@yandex.ru>
2022-07-11 10:44:39 +0300

    3. Бот [#7921]

--- chapter_003Input_Output/src/main/java/echoServer/EchoServer.java
+++ chapter_003Input_Output/src/main/java/echoServer/EchoServer.java
@@ -13,14 +13,29 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if(str.contains("/?msg=Bye")){
-                            server.close();
+                        if(str.contains("/?msg=Hello")){
+                            out.write("Hello, dear friend.".getBytes());
+                            System.out.println("Hello, dear friend");
+                            break;
                         }
-                    }
-                    if(server.isClosed()) {
-                        System.out.println("server shut down");
+                        if(str.contains("/?msg=Exit")){
+                            server.close();
+                            out.write("Server shut down".getBytes());
+                            System.out.println("Server shut down");
+                            break;
+                        }
+                        if (str.matches("[GET /?msg=].+[HTTP/1.1]")) {
+                            int start = str.indexOf("=") + 1;
+                            int end = str.indexOf("HTTP");
+                            out.write(str.substring(start, end).getBytes());
+                            System.out.println(str.substring(start, end));
+                            break;
+                        }
+
                     }
                     out.flush();
                 }
