98193d80949407178ed8ad983535e9749439db1f
pavel.chupin <pavelchupin94@yandex.ru>
2022-07-21 16:14:46 +0300

    1. Inner join [#373617]

--- chapter_004_SQL_JDBC/SQL script/Inner join/join.sql
+++ chapter_004_SQL_JDBC/SQL script/Inner join/join.sql
@@ -1,19 +1,29 @@
 CREATE table teachers (
 	id serial primary key,
-	fio varchar(255) NULL,
-	lesson_id int references lessons(id),
-	experience int
+	fio varchar(255),
+	experience int NULL
 );
 
 CREATE table lessons (
     id serial primary key,
     name varchar(255),
-    academic_hours int
+    academic_hours int NULL,
+    teacher_id references teachers(id) NULL
 );
 
 
 
 
-select * from teachers as t  join lessons  as l  on t.lesson_id  = l.id;
-select t.fio, l.name, l.academic_hours  from teachers as t  join lessons  as l  on t.lesson_id  = l.id and l.name = 'Английский';
-select t.fio as "ФИО", l.name as "Предмет", t.experience as "Срок преподавания"  from teachers as t  join lessons  as l  on t.lesson_id  = l.id and t.experience >5;
+select * 
+from teachers as t  join lessons  as l  
+on t.id  = l.teacher_id;
+
+select t.fio, l.name, l.academic_hours  
+from teachers as t  join lessons  as l  
+on t.id  = l.teacher_id 
+where l.name = 'Английский';
+
+select t.fio as "ФИО", l.name as "Предмет", t.experience as "Срок преподавания"  
+from teachers as t  join lessons  as l  
+on t.id  = l.teacher_id 
+where t.experience >5;
