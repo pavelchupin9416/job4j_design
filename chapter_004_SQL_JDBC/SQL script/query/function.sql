2598e872cbba2947e76a19dc57ac96248ce6003f
pavel.chupin <pavelchupin94@yandex.ru>
2022-07-21 17:38:47 +0300

    фикс 2. Группировка и агрегатные функции [#373618 #324557]

--- chapter_004_SQL_JDBC/SQL script/query/function.sql
+++ chapter_004_SQL_JDBC/SQL script/query/function.sql
@@ -1,6 +1,40 @@
+INSERT INTO people ("name") VALUES
+	 ('Pavel'),
+	 ('Viktor'),
+	 ('Nikolai'),
+	 ('Olga'),
+	 ('Jon'),
+	 ('Tom'),
+	 ('Ivan');
+
+INSERT INTO devices ("name",price) VALUES
+	 ('Poco m3 pro',18000.0),
+	 ('Poco x3',21400.0),
+	 ('Boss rc 300',38000.0),
+	 ('Boss rc 600',45670.0),
+	 ('Hotone ampero',23130.0),
+	 ('Alesis nitro mesh kit',66000.0),
+	 ('Alesis strike pro',180344.0),
+	 ('Ammon stereo looper',5600.0);
+
+
+
+INSERT INTO devices_people (device_id,people_id) VALUES
+	 (1,5),
+	 (6,1),
+	 (1,3),
+	 (2,4),
+	 (5,7),
+	 (5,1),
+	 (6,2),
+	 (5,2),
+	 (8,6),
+	 (7,3),
+	 (8,1),
+	 (2,2);
+
 select avg(price) as "Средняя цена устройства"
 from devices;
-
 
 select p.name as "Человек", avg(d.price) as "Средняя цена устройства"
 from devices_people as d_p join devices as d 
