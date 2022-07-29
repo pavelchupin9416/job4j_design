71f152573d49524622e7aa82d439c7a8ede1d1b7
pavel.chupin <pavelchupin94@yandex.ru>
2022-07-29 10:06:22 +0300

    0. JDBC [#6863]

---
+++ chapter_004_SQL_JDBC/src/main/java/jdbc/ConnectionDemo.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import config.*;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+       Config config = new Config("chapter_004_SQL_JDBC\\src\\main\\resources\\app.properties");
+        config.load();
+        Class.forName(config.value("connection.driver_class"));
+        String url = config.value("connection.url");;
+        String login =  config.value("connection.username");
+        String password = config.value("connection.password");;
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
